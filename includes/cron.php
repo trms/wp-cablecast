4f0867786b873367cbc68633b8c260fda015e61b
raytiley <raytiley@gmail.com>
2017-01-28 13:42:19 -0500

    Default cron is every 30 minutes

--- includes/cron.php
+++ includes/cron.php
@@ -2,9 +2,9 @@
 
 function cablecast_cron_schedules( $schedules ) {
 	// add a 'weekly' schedule to the existing set
-	$schedules['5-seconds'] = array(
-		'interval' => 5,
-		'display' => __('Once Weekly')
+	$schedules['30-minutes'] = array(
+		'interval' => 30 * 60,
+		'display' => __('Every 30 Minutes')
 	);
 	return $schedules;
 }
@@ -13,5 +13,5 @@ add_filter( 'cron_schedules', 'cablecast_cron_schedules' );
 add_action( 'cablecast_cron_hook', 'cablecast_sync_data' );
 
 if ( ! wp_next_scheduled( 'cablecast_cron_hook' ) ) {
-  wp_schedule_event( time(), '5-seconds', 'cablecast_cron_hook' );
+  wp_schedule_event( time(), '30-minutes', 'cablecast_cron_hook' );
 }
