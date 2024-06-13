7717a240e1d4d917cbce760659f356272c877ee0
amanda-gerold <61241785+amanda-gerold@users.noreply.github.com>
2024-06-13 14:27:47 -0500

    Display adjustments
    Rearranged the title and prev/next buttons so they fall under the video. Also added some css classes so I could call out the right items with css for styling.

--- display.php
+++ display.php
@@ -70,16 +70,23 @@ function cablecast_content_display($content){
       $next_date = date('Y-m-d', strtotime($date . "+1days"));
       $prev_link = add_query_arg(array('schedule_date' => $prev_date));
       $next_link = add_query_arg(array('schedule_date' => $next_date));
-
+      
       $schedule_itmes = cablecast_get_schedules($channel_id, $date);
 
-      $schedule_content = "<h3>Schedule For $date</h3>";
       $channel_embed_code = get_post_meta($post->ID, 'cablecast_channel_live_embed_code', true);
       if (empty($channel_embed_code) == false) {
         $schedule_content .= "<div class=\"wrap\">$channel_embed_code</div>";
       }
-      $schedule_content .= "<a href=\"$prev_link\">Previous</a> | <a href=\"$next_link\">Next</a>";
-      $schedule_content .= "<table><thead><tr><th>Time</th><th>Show</th></tr></thead><tbody>";
+      
+      $schedule_content .= "
+        <div class=\"schedule-title-container\">
+          <h3>Schedule For $date</h3> 
+          <div class=\"schedule-prev-next-btns\">
+            <a href=\"$prev_link\" class=\"!text-brand-accent hover:underline\">Previous</a> | <a href=\"$next_link\" class=\"!text-brand-accent hover:underline\">Next</a>
+          </div>
+        </div>";
+
+      $schedule_content .= "<table><thead><tr><th class=\"schedule-time\">Time</th><th class=\"schedule-show\">Show</th></tr></thead><tbody>";
       foreach($schedule_itmes as $item) {
         $show_link = get_post_permalink($item->show_post_id);
         if (empty($show_link)) { continue; }
